e345fd435596229d7f2568c04021f33d96b0d423
Nikolosin <57622487+Nikolosin@users.noreply.github.com>
2020-09-24 18:19:38 +0300

    Update MainActivity.java
    try whis resources

--- app/src/main/java/com/example/savetxt/MainActivity.java
+++ app/src/main/java/com/example/savetxt/MainActivity.java
@@ -30,23 +30,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final File logFile = new File(getApplicationContext().getExternalFilesDir(null),"Sample.txt");
-        try {
-            String [] sample = getResources().getStringArray(R.array.sample);
-            FileWriter logWriter = new FileWriter(logFile);
-            for (String s : sample) {
-                logWriter.append(s + ";");
+        if (logFile.length() == 0) {
+            try (FileWriter logWriter = new FileWriter(logFile)) {
+                String[] sample = getResources().getStringArray(R.array.sample);
+                for (String s : sample) {
+                    logWriter.append(s).append(";");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            logWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         loadSample(logFile);
         Button btnAdd = findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    FileWriter logWriterName = new FileWriter(logFile, true);
+                try (FileWriter logWriterName = new FileWriter(logFile, true);){
                     BufferedWriter bufferWriter = new BufferedWriter(logWriterName);
                     bufferWriter.write(getString(R.string.nameAuthor)+";");
                     bufferWriter.close();
@@ -71,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
             final SampleAdapter adapter = new SampleAdapter(this, data);
             ListView list = findViewById(R.id.list_sample);
             list.setAdapter(adapter);
+            logReader.close();
+            reader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
